81dbb86bd9094ab9466cbad3047e65c79322e76d
AlmirPaulo <ap.freelas@gmail.com>
2021-08-04 20:16:19 -0300

    cli

--- server.py
+++ server.py
@@ -1,29 +1,10 @@
 from flask import Flask, request
-import simple_websocket, random
+import simple_websocket, random, logging, pdb
 
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
-class Player1:
-    def __init__(self, color, room):
-        self.color = random.choice(['white','black'])
-        self.room = random.randint(1000, 9999)
-
-class Player2:
-    def __init__(self, color, room):
-        self.color = color
-        self.room = room
-        
-
-    def get_room(self, room):
-        self.room = Player1().room
-
-
-    def get_color(self, color):
-        if Player1().color == 'white':
-            self.color = 'black'
-        elif Player1().color == 'black': 
-            self.color = 'white'
 
 @app.route('/')
 def ping():
@@ -32,44 +13,56 @@ def ping():
 @app.route('/game', websocket=True)
 def game_loop():
     socket = simple_websocket.Server(request.environ)
-    #Setting Game
-    try:
-        while True:
-            socket.receive()
-            set_player = 'Start Game?'
-            socket.send(set_player)
-            if socket.receive(600).lower() == 'yes':
-                Player1()
-                socket.send('Wait for player 2...')
-            socket.send(set_player)
-            if socket.receive(600).lower() == 'yes':
-                Player2().get_room()
-                Player2().get_color()
-    except simple_websocket.ConnectionClosed:
-        pass
 
-    #Game Mechanics
+    #Start Game
+    players = []
     try:
-        while True:
-            move = socket.receive()
-            #randomize player color
-                #check if it is legal move
-                    #move
-                    #report ilegal move
-                        #check if it is check
-                            #report check
-                        #check if it is checkmate
-                            #Report check mate
-                            # end game
-                        #check if it is stalemate
-                            #report stalemate
-                            #end game
-                    #Pass turn
-            socket.send(resp)
+        player = socket.receive(300)
+        player = player.partition('/')
+        players.append()
+        try:
+            player1 = players[-1].partition('/')
+            if player1[2] == player2[2]:
+                if player1[0] == 'white' and player2[0] == 'white':
+                    player2_color = 'black'
+                    player1_color = 'white'
+                    logging.debug(player2_color, player1_color)
+                   # game_mechanics()
+                elif player1[0] == 'black' and player2[0] == 'black':
+                   player2_color = 'white'
+                   player1_color = 'black'
+                   logging.debug(player2_color, player1_color)
+                   # game_mechanics()
+                else:
+                   player1_color = player1[0]
+                   player2_color = player2[0]
+                   logging.debug(player2_color, player1_color)
+                   # game_mechanics()
+        except:
+            socket.send('Waiting for a second player...')
+        pdb.set_trace()
+
     except simple_websocket.ConnectionClosed:
         pass
     return ''
 
+
+def game_mechanics():
+    while True:
+        move = socket.receive(600)
+        #check if it is legal move
+            #move
+            #report ilegal move
+                #check if it is check
+                    #report check
+                #check if it is checkmate
+                    #Report check mate
+                    # end game
+                #check if it is stalemate
+                    #report stalemate
+                    #end game
+            #Pass turn
+        socket.send(resp)
 
 if __name__ == '__main__':
     app.run(debug=True)
